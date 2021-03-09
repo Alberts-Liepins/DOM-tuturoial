6024cba5330d5c3d64cffa33fac445d58b0f3003
Alberts-Liepins <liepins01@example.com>
2021-03-09 15:00:52 +0200

    #6 completed

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-var books = document.querySelectorAll('#book-list li .name')
+const banner = document.querySelector('#page-banner')
 
-Array.from(books).forEach(function(book){
-    book.textContent += ' (book title)'
-})
+console.log('#page-banner node type is:', banner.nodeType)
+console.log('#page-banner node name is:', banner.nodeName)
+console.log('#page-banner has child nodes:', banner.hasChildNodes())
 
-const BookList = document.querySelector('#book-list')
-//BookList.innerHTML = '<h2>Books and more books...</h2>'
-BookList.innerHTML += '<p>This is how you add HTML</p>'
+const clonedBanner = banner.cloneNode(true)
+console.log(clonedBanner)
